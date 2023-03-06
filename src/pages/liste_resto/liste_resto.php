f1572a42555317329f439d48cf66b8d3ccfe14ef
Arseid <yvesrobert.estrada@gmail.com>
2023-03-06 16:51:50 +0100

    save

---
+++ src/pages/liste_resto/liste_resto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Liste des restaurants</title>
+    </head>
+    <body>
+        <h1>Liste des restaurants</h1>
+        <ul>
+            <li>
+                <span>ID</span>
+            </li>
+            <li>
+                <span>Nom</span>
+            </li>
+            <li>
+                <span>Cuisine</span>
+            </li>
+            <li>
+                <span>Arrondissement</span>
+            </li>
+            <li>
+                <span>Code Postal</span>
+            </li>
+        </ul>
+    </body>
+</html>
